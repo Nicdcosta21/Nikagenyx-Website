b7d81376cbe32535b25d4443e76593e0fb46fbbc
Nicdcosta21 <nic.dcosta21@gmail.com>
2025-07-01 02:06:39 +0300

    Update verifySession.js

--- netlify/functions/verifySession.js
+++ netlify/functions/verifySession.js
@@ -1,7 +1,16 @@
 const jwt = require('jsonwebtoken');
+const cookie = require('cookie');
 
-module.exports = (event) => {
-  const m = (event.headers.cookie || '').match(/nikagenyx_session=([^;]+)/);
-  if (!m)               throw new Error('missing');
-  return jwt.verify(m[1], process.env.JWT_SECRET);   // throws if expired/tampered
+module.exports = function verify(event) {
+  const cookies = cookie.parse(event.headers.cookie || '');
+  const token = cookies.nikagenyx_session;
+
+  if (!token) throw new Error('No token');
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded; // useful if you want to access empId later
+  } catch (err) {
+    throw new Error('Invalid token');
+  }
 };
