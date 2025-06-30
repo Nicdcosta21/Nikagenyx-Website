b959ed458ffc5077c8946a3e35a5ed0be80ac941
Nicdcosta21 <nic.dcosta21@gmail.com>
2025-07-01 02:33:39 +0300

    Delete netlify/functions/auth-gate.js

--- netlify/functions/auth-gate.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  function hasSession () {
-    return document.cookie.split('; ')
-      .some(p => p.startsWith('nikagenyx_session='));
-  }
-  if (!hasSession()) window.location.replace('/employee_portal.html');
-})();
