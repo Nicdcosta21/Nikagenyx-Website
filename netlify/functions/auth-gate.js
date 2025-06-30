c886fab2468cba66b3d5d593017404638b6d4368
Nicdcosta21 <nic.dcosta21@gmail.com>
2025-07-01 02:29:35 +0300

    Create auth-gate.js

---
+++ netlify/functions/auth-gate.js
@@ -0,0 +1,7 @@
+(function () {
+  function hasSession () {
+    return document.cookie.split('; ')
+      .some(p => p.startsWith('nikagenyx_session='));
+  }
+  if (!hasSession()) window.location.replace('/employee_portal.html');
+})();
