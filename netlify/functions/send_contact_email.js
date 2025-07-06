5bea600de8ed8a05cdceefe886bc3bd3907d2e1b
Nicdcosta21 <nic.dcosta21@gmail.com>
2025-07-06 18:02:58 +0200

    Update send_contact_email.js

--- netlify/functions/send_contact_email.js
+++ netlify/functions/send_contact_email.js
@@ -34,12 +34,14 @@ exports.handler = async (event) => {
   ];
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+  host: "smtpout.secureserver.net",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
   try {
     // Send internal team email
