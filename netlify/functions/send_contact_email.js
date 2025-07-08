15d8ec190bbc363a065c9741baa20a20c82e604e
Nicdcosta21 <nic.dcosta21@gmail.com>
2025-07-08 20:46:28 +0200

    Update send_contact_email.js

--- netlify/functions/send_contact_email.js
+++ netlify/functions/send_contact_email.js
@@ -41,8 +41,8 @@ exports.handler = async (event) => {
     port: 465,
     secure: true,
     auth: {
-      user: process.env.EMAIL_USER, // consultant@nikagenyx.com
-      pass: process.env.EMAIL_PASS  // NGXConsultant@001
+      user: process.env.EMAIL_USER, // 
+      pass: process.env.EMAIL_PASS  // 
     }
   });
 
