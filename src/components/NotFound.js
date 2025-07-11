6c6216c1da22f8e01a727f1479626a7cb2b5ca4a
Nicdcosta21 <nic.dcosta21@gmail.com>
2025-07-11 18:55:08 +0200

    Create NotFound.js

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+// src/components/NotFound.js
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">404 – Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
