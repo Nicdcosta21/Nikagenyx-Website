5b80daa80cb9ab7526bee45aaa878709118e5dc8
Nicdcosta21 <nic.dcosta21@gmail.com>
2025-07-11 17:17:50 +0200

    Create site.js

---
+++ src/config/site.js
@@ -0,0 +1,10 @@
+const config = {
+  title: 'Nikagenyx Vision Tech',
+  description: 'Built for the Moment. Designed for Momentum.',
+  url: 'https://www.nikagenyx.com',
+  apiBaseUrl: 'https://www.nikagenyx.com/api',
+  contactEmail: 'support@nikagenyx.com',
+  
+};
+
+export default config;
