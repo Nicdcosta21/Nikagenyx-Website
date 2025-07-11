a84ab2ca823b697a1ac3e86974d53b358e4915cd
Nicdcosta21 <nic.dcosta21@gmail.com>
2025-07-11 17:22:44 +0200

    Update site.js

--- src/config/site.js
+++ src/config/site.js
@@ -4,7 +4,15 @@ const config = {
   url: 'https://www.nikagenyx.com',
   apiBaseUrl: 'https://www.nikagenyx.com/api',
   contactEmail: 'support@nikagenyx.com',
-  
+  logo: {
+    main: '/assets/logo_original.png',
+    favicon: '/assets/logo_original.png'
+  },
+  meta: {
+    themeColor: '#000000',
+    twitterCard: 'summary_large_image',
+    ogType: 'website'
+  }
 };
 
 export default config;
